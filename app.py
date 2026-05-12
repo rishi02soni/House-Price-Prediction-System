2767167703c1495559c7a67c8065ea156009345d
Rishi Soni <167216843+rishi02soni@users.noreply.github.com>
2026-05-12 22:52:39 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+
+model = pickle.load(open("model.pkl", "rb"))
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    area = int(request.form['area'])
+    bedrooms = int(request.form['bedrooms'])
+    bathrooms = int(request.form['bathrooms'])
+
+    prediction = model.predict([[area, bedrooms, bathrooms]])
+
+    return render_template("index.html", prediction_text=f'Estimated Price: ₹{int(prediction[0])}')
+
+if __name__ == "__main__":
+    app.run(debug=True)
